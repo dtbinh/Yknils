0477d4faeb12fe8691c5cb4a8cf92366f78e4e9f
Ronnie McGregor <rmcgr467@mtroyal.ca>
2015-02-24 12:06:20 -0700

    Initial prototype

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Run Yknils");
+		//Button.ENTER.waitForPress();
+		DifferentialPilot p = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.B);
+		p.setTravelSpeed(2);
+		p.travel(-9);
 	}
 
 }
